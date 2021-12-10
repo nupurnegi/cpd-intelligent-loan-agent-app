6ac6f05c50b40703725cb50030d872ce0c74832f
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2021-12-10 12:40:37 +0530

    Update create-ibm-cloud-services.sh

--- create-ibm-cloud-services.sh
+++ create-ibm-cloud-services.sh
@@ -2,21 +2,21 @@ servicename="cp-cos"
 service="cloud-object-storage"
 region="global"
 ibmcloud resource service-instance-create $servicename $service lite $region
-ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
-apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
-url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
-JSON_STRING='{"apikey":"'"$apikey"'","url":"'"$url"'"}'
-echo $JSON_STRING > cos.json
+# ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
+# apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
+# url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
+# JSON_STRING='{"apikey":"'"$apikey"'","url":"'"$url"'"}'
+# echo $JSON_STRING > cos.json
 
 servicename="cp-wml"
 service="pm-20"
 region="us-south"
 ibmcloud resource service-instance-create $servicename $service lite $region
-ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
-apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
-url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
-JSON_STRING='{"apikey":"'"$apikey"'","url":"'"$url"'"}'
-echo $JSON_STRING > watsonml.json
+# ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
+# apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
+# url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
+# JSON_STRING='{"apikey":"'"$apikey"'","url":"'"$url"'"}'
+# echo $JSON_STRING > watsonml.json
 
 # servicename="cp-wstudio"
 # service="data-science-experience"

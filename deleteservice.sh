3253a81f09d33588e2d76fe1885442a9784c156a
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2021-12-10 12:26:54 +0530

    Update deleteservice.sh

--- deleteservice.sh
+++ deleteservice.sh
@@ -1,24 +1,18 @@
 
-# servicename="cp-stt"
-# ibmcloud resource service-key-delete "$servicename-creds" -f
-# ibmcloud resource service-instance-delete $servicename -f
-# rm speechtotext.json
-# rm cp-stt.txt
+servicename="cp-cos"
+ibmcloud resource service-key-delete "$servicename-creds" -f
+ibmcloud resource service-instance-delete $servicename -f
+rm cos".json
+rm cp-cos".txt
 
-# servicename="cp-wa"
-# ibmcloud resource service-key-delete "$servicename-creds" -f
-# ibmcloud resource service-instance-delete $servicename -f
-# rm watsonassistant.json
-# rm cp-wa.txt
+servicename="cp-wml"
+ibmcloud resource service-key-delete "$servicename-creds" -f
+ibmcloud resource service-instance-delete $servicename -f
+rm watsonml.json
+rm cp-wml.txt
 
-# servicename="cp-wd"
-# ibmcloud resource service-key-delete "$servicename-creds" -f
-# ibmcloud resource service-instance-delete $servicename -f
-# rm watsondiscovery.json
-# rm cp-wd.txt
-
-# servicename="cp-cdb"
-# ibmcloud resource service-key-delete "$servicename-creds" -f
-# ibmcloud resource service-instance-delete $servicename -f
-# rm cloudant.json
-# rm cp-cdb.txt
+servicename="cp-wstudio"
+ibmcloud resource service-key-delete "$servicename-creds" -f
+ibmcloud resource service-instance-delete $servicename -f
+rm watsonstudio.json
+rm cp-wstudio.txt

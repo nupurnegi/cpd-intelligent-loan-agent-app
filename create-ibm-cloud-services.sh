8eda0dd07762e70e210aa1b6e2ff2e0a86079ac7
Nupur Negi <35726589+nupurnegi@users.noreply.github.com>
2022-01-18 17:28:29 +0530

    Update create-ibm-cloud-services.sh

--- create-ibm-cloud-services.sh
+++ create-ibm-cloud-services.sh
@@ -18,10 +18,10 @@ ibmcloud resource service-instance-create $servicename $service lite $region
 # JSON_STRING='{"apikey":"'"$apikey"'","url":"'"$url"'"}'
 # echo $JSON_STRING > watsonml.json
 
-servicename="cp-wstudio"
-service="data-science-experience"
-region="us-south"
-ibmcloud resource service-instance-create $servicename $service free-v1 $region
+# servicename="cp-wstudio"
+# service="data-science-experience"
+# region="us-south"
+# ibmcloud resource service-instance-create $servicename $service free-v1 $region
 # ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
 # apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
 # url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
